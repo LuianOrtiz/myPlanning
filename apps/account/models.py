85ccbcc5b6506473498a6105b95c1a3726fa2b44
Luian Ortiz <luiann.ortiz@gmail.com>
2022-06-23 21:42:11 -0500

    add account's model

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Memberhsip(models.Model):
+    pass
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    phone = models.PhoneNumberField()
+    email = models.EmailField(max_length=254)
+    date_created = models.DateTimeField(auto_now_add=True)
