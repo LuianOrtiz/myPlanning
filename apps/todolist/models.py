23669cbdc32fc6ae6df8b8328df5c7c40217565f
Luian Ortiz <luiann.ortiz@gmail.com>
2022-06-23 21:11:05 -0500

    add str functions to models in todolist

--- apps/todolist/models.py
+++ apps/todolist/models.py
@@ -7,6 +7,10 @@ from django.forms import CharField
 
 class List(models.Model):
     list_name = models.CharField(blank=False, default= '', max_length=60)
+    
+    def __str__(self):
+        return self.list_name
+    
 
 class Task(models.Model):
     list = models.ForeignKey(List, on_delete=models.CASCADE)
@@ -15,8 +19,16 @@ class Task(models.Model):
     created = models.DateField(auto_now_add=True)
     finalized = models.DateField(null=True)
 
+    def __str__(self):
+        return self.task
+    
+
 class Step(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     step = models.CharField(blank=False, default= '', max_length=180)
     completed = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.step
+    
